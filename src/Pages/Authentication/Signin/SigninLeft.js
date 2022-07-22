5ea16141063bebba4a561c3f130b685049e711bd
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-07-22 11:39:32 +0600

    change code

--- src/Pages/Authentication/Signin/SigninLeft.js
+++ src/Pages/Authentication/Signin/SigninLeft.js
@@ -6,7 +6,6 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import {
-  useAuthState,
   useSignInWithEmailAndPassword,
   useSignInWithFacebook,
   useSignInWithGithub,
